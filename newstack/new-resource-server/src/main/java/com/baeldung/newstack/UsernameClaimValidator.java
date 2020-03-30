9e5eaba70d3edb5db6cf6ca41d729d0c5d891ce8
Shirish <shirishsharma16@gmail.com>
2020-03-30 23:54:41 +0530

    JWT custom claim validator

---
+++ newstack/new-resource-server/src/main/java/com/baeldung/newstack/UsernameClaimValidator.java
@@ -0,0 +1,23 @@
+package com.baeldung.newstack;
+
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+public class UsernameClaimValidator implements OAuth2TokenValidator<Jwt> {
+
+	@Override
+	public OAuth2TokenValidatorResult validate(Jwt token) {
+		OAuth2Error error = new OAuth2Error("invalid_claim");
+
+		final String username = (String) token.getClaim("preferred_username");
+		String[] splitUsername = username.split("@");
+		if(splitUsername[1].equals("test.com")) {
+			return	OAuth2TokenValidatorResult.success();
+		}
+		else {
+			return OAuth2TokenValidatorResult.failure(error);
+		}
+	}
+}
--- newstack/new-resource-server/src/main/java/com/baeldung/newstack/spring/SecurityConfig.java
+++ newstack/new-resource-server/src/main/java/com/baeldung/newstack/spring/SecurityConfig.java
@@ -1,14 +1,29 @@
 package com.baeldung.newstack.spring;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import com.baeldung.newstack.UsernameClaimValidator;
+import com.nimbusds.jwt.proc.JWTClaimsSetVerifier;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
+	
+	@Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+	private String issuerUri;
+	
+	@Override
     protected void configure(HttpSecurity http) throws Exception {// @formatter:off
         http.authorizeRequests()
               .antMatchers(HttpMethod.GET, "/user/info", "/api/projects/**")
@@ -21,5 +36,26 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
               .oauth2ResourceServer()
                 .jwt();
     }//@formatter:on
+    
+    
+    @Bean
+    JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder)
+             //   JwtDecoders.fromOidcIssuerLocation("http://localhost:8083/auth/realms/baeldung");
+        		JwtDecoders.fromOidcIssuerLocation(issuerUri);
+
+       // OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer("http://localhost:8083/auth/realms/baeldung");
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, getCustomJwtClaimVerifier());
+
+        jwtDecoder.setJwtValidator(withAudience);
+
+        return jwtDecoder;
+    }
+   
+    @Bean
+    public OAuth2TokenValidator<Jwt> getCustomJwtClaimVerifier() {
+        return new UsernameClaimValidator();
+    }
 
 }
